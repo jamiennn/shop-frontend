52330bc5df1f7e37cf9b1c57df59e7ae172e0e5c
jamiennn <jasper07165@hotmail.com>
2023-09-05 23:23:17 +0800

    fix: bugs in .yml

--- test/e2e/tests/login.spec.js
+++ test/e2e/tests/login.spec.js
@@ -14,7 +14,6 @@ const json = {
 }
 
 test('try to login', async ({ page }) => {
-  console.log(baseUrl)
   await page.route(`${baseUrl}/users/login`, async route => {
     await route.fulfill({ json })
   })
