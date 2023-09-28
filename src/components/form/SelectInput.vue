d1e4a0d319ff74170064958df5cb764f4550a9f9
jamiennn <jasper07165@hotmail.com>
2023-09-28 12:18:05 +0800

    refactor: select input component'

--- src/components/form/SelectInput.vue
+++ src/components/form/SelectInput.vue
@@ -1,28 +1,17 @@
 <script setup lang="ts">
-import { inject, onMounted, ref, computed } from 'vue'
-import { getCategoriesApi } from '@/api/category.js'
-
+import { inject, computed } from 'vue'
 
 defineProps<{
   name: string,
   nameCn: string,
+  options: Array<any>,
   modelValue: string,
-  categoryInput: number
+  selectedInput: number
 }>()
 const emit = defineEmits(['update:modelValue'])
-const categories = ref()
-let status = inject('status')
+let status: any = inject('status')
 
-onMounted(async () => {
-  const data = await getCategoriesApi()
-  if (data.success) {
-    categories.value = data.messages.categories
-  } else {
-    console.error(data)
-  }
-})
-
-function handleSelect(e) {
+function handleSelect(e: any) {
   status.value = 'typing'
   return emit('update:modelValue', e.target.value)
 }
@@ -40,9 +29,8 @@ const selectClass = computed(() => {
   <label :for="name" class="form-label">{{ nameCn }}：</label>
   <select :class="selectClass" :id="name" @change="handleSelect" :disabled="status === 'submitting'">
     <option disabled selected>選擇{{ nameCn }}</option>
-    <option v-for="(category, index) in categories" :key="index" :value="category.id"
-      :selected="category.id === categoryInput">
-      {{ category.name }}</option>
+    <option v-for="(option, index) in options" :key="index" :value="option.id" :selected="option.id === selectedInput">
+      {{ option.name }}</option>
   </select>
 </template>
 
